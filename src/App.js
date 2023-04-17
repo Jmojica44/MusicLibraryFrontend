156f86f6e302cb504d7abfec5029e5c67f738b14
Jonathan Mojica <Jonathan.Mojica44@gmail.com>
2023-04-17 16:10:52 -0500

    added in component AddNewSong

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+  async function getAllSongs() {
+    let response = await axios.get('http://127.0.0.1:5000/api/songs/');
+    setSongs(response.data);
+  }
+
+  async function addNewSong(newSong) {
+    let response = await axios.post('http://127.0.0.1:5000/api/songs/', newSong);
+    if(response.status === 201) {
+      await getAllSongs();
+    }
+  }
+
+  const filterSongs = (e) => {
+    let filterValue = e.target.value
+  }
+
+
+  return(
+<p class='app-link'> Hello World</p>
+  )
 }
 
 export default App;
